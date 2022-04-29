1946fdd31051a7868fed0979302c162c9fbfa794
Danny Linhardt <daniel.linhardt@gmail.com>
2022-04-28 20:59:47 -0400

    commit tree c4699c538f8817b07ae7bde412ee2db3fa4f1654

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  sayHelloToGuadalupe();
+  
